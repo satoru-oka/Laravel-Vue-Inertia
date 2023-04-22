dc2cc74c86b86854a685805338539d87517a074f
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-22 14:25:19 +0900

    Layouts

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>Show</div>
-    <Link href="/">MainPage</Link>
+    <MainLayout>
+        <div>Show</div>
+        <Link href="/">MainPage</Link>
+    </MainLayout>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/inertia-vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue'
 </script>
